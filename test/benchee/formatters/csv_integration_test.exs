06174b250ccf1254e77df890458e96e2d30e773c
Tobias Pfeiffer <pragtob@gmail.com>
2016-07-11 19:07:41 +0200

    Integration reads CSV and always cleans up

--- test/benchee/formatters/csv_integration_test.exs
+++ test/benchee/formatters/csv_integration_test.exs
@@ -4,19 +4,30 @@ defmodule Benchee.Formatters.CSVIntegrationTest do
 
   @file_path "test.csv"
   test "works just fine" do
-    capture_io fn ->
-      Benchee.run %{
-        time: 0.01,
-        warmup: 0.02,
-        formatters: [&Benchee.Formatters.CSV.output/1],
-        csv: %{file: @file_path}
-      },
-      %{
-        "Sleep"        => fn -> :timer.sleep(10) end,
-        "Sleep longer" => fn -> :timer.sleep(20) end
-      }
+    try do
+      capture_io fn ->
+        Benchee.run %{
+          time: 0.01,
+          warmup: 0.02,
+          formatters: [&Benchee.Formatters.CSV.output/1],
+          csv: %{file: @file_path}
+        },
+        %{
+          "Sleep"        => fn -> :timer.sleep(10) end,
+          "Sleep longer" => fn -> :timer.sleep(20) end
+        }
 
-      assert File.exists?(@file_path)
+        assert File.exists?(@file_path)
+
+        [header, _row_1, _row_2] = @file_path
+                                 |> File.stream!
+                                 |> CSV.decode
+                                 |> Enum.take(3)
+
+        [first_header | _rest] = header
+        assert first_header == "Name"
+      end
+    after
       File.rm! @file_path
     end
   end
