a89f18930c71b54892d77bdf09ef34bfdffddfb5
Tobias Pfeiffer <pragtob@gmail.com>
2016-07-11 18:58:51 +0200

    Basic integration test and adjustments

--- lib/benchee/formatters/csv.ex
+++ lib/benchee/formatters/csv.ex
@@ -28,14 +28,15 @@ defmodule Benchee.Formatters.CSV do
 
   ## Examples
 
-      iex> [{"My Job", %{average: 200.0, ips: 5000.0, std_dev: 20, std_dev_ratio: 0.1, std_dev_ips: 500, median: 190.0}}]
+      iex> suite = %{statistics: %{"My Job" => %{average: 200.0, ips: 5000.0, std_dev: 20, std_dev_ratio: 0.1, std_dev_ips: 500, median: 190.0}}}
+      iex> suite
       iex> |> Benchee.Formatters.CSV.format
       iex> |> Enum.take(2)
       ["Name,Iterations per Second,Average,Standard Deviation,Standard Deviation Iterations Per Second,Standard Deviation Ratio,Median\\r\\n",
        "My Job,5.0e3,200.0,20,500,0.1,190.0\\r\\n"]
 
   """
-  def format(jobs) do
+  def format(%{statistics: jobs}) do
     sorted = Benchee.Statistics.sort(jobs)
     [@column_descriptors | job_csvs(sorted)]
     |> CSV.encode
