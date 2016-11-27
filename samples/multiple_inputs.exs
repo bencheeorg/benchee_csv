b90c079367e23d270103d416faedc6d433d71514
Tobias Pfeiffer <pragtob@gmail.com>
2016-11-27 10:50:34 +0100

    Multiple inputs sample

---
+++ samples/multiple_inputs.exs
@@ -0,0 +1,18 @@
+# It is possible to use multiple formatters so that you have both the Console
+# output and a csv file.
+map_fun = fn(i) -> [i, i * i] end
+
+Benchee.run(%{
+  "flat_map"    => fn(list) -> Enum.flat_map(list, map_fun) end,
+  "map.flatten" => fn(list) -> list |> Enum.map(map_fun) |> List.flatten end
+},
+  formatters: [
+    &Benchee.Formatters.CSV.output/1,
+    &Benchee.Formatters.Console.output/1
+  ],
+  csv: [file: "my.csv"],
+  inputs: %{
+    "Small" => Enum.to_list(1..1_000),
+    "Bigger" => Enum.to_list(1..100_000)
+  }
+)
