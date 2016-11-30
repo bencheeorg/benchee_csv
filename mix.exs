17e98b019de4cf243c1c998f246ce3c8a0d9f32c
Tobias Pfeiffer <pragtob@gmail.com>
2016-11-30 19:24:24 +0100

    Depend on released benchee version

--- mix.exs
+++ mix.exs
@@ -26,7 +26,7 @@ defmodule BencheeCSV.Mixfile do
 
   defp deps do
     [
-      {:benchee, "~> 0.6", git: "git@github.com:PragTob/benchee.git"},
+      {:benchee, "~> 0.6"},
       {:csv,     "~> 1.4.0"},
       {:credo,   "~> 0.5",  only: :dev},
       {:ex_doc,  "~> 0.11", only: :dev},
