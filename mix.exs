e87ce6b0d7d98408edd8d516c3f4baf4147f0a43
Tobias Pfeiffer <pragtob@gmail.com>
2017-05-03 21:56:58 +0200

    Ok travis wont work with :git, so :github it is

--- mix.exs
+++ mix.exs
@@ -31,7 +31,7 @@ defmodule BencheeCSV.Mixfile do
 
   defp deps do
     [
-      {:benchee,     "~> 0.6", git: "git@github.com:PragTob/benchee.git"},
+      {:benchee,     "~> 0.6", github: "PragTob/benchee"},
       {:csv,         "~> 1.4.0"},
       {:excoveralls, "~> 0.6.1",  only: :test},
       {:credo,       "~> 0.5",    only: :dev},
