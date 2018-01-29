4b2ba41167aebf2b97607cdca4fffb4e8a8f7c00
Tobias Pfeiffer <pragtob@gmail.com>
2018-01-29 20:44:28 +0100

    forgot mix.exs bump

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule BencheeCSV.Mixfile do
   def project do
     [app: :benchee_csv,
      version: @version,
-     elixir: "~> 1.2",
+     elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps(),
