598eb9131939e90c6d5125059b8b267b651cd34d
Tobias Pfeiffer <pragtob@gmail.com>
2018-02-11 11:44:32 +0100

    Switch it around so that values are in the columns as they should be
    (when I did a trial run with the old implementation libre office
    said it couldn't open it because too many values in columns which
    was the sign that we probably should switch :D)

---
+++ lib/benchee/formatters/csv/util.ex
@@ -0,0 +1,48 @@
+defmodule Benchee.Formatters.CSV.Util do
+  @moduledoc false
+
+  @doc """
+  Contrary to `List.zip/1` zips all elements of the list together and doesn't
+  stop as soon as one of the lists doesn't have any elements anymore.
+
+  Missing elements are filled up with `nil`.
+
+
+  ## Examples
+
+      iex> Benchee.Formatters.CSV.Util.zip_all([[1, 2], [3, 4], [5, 6]])
+      [[1, 3, 5], [2, 4, 6]]
+
+      iex> Benchee.Formatters.CSV.Util.zip_all([[1, 2, 3], [3, 4], [5, 6]])
+      [[1, 3, 5], [2, 4, 6], [3, nil, nil]]
+
+      iex> Benchee.Formatters.CSV.Util.zip_all([[1, 2, 3], [3, 4], [5, 6, 7, 8]])
+      [[1, 3, 5], [2, 4, 6], [3, nil, 7], [nil, nil, 8]]
+
+      iex> Benchee.Formatters.CSV.Util.zip_all([[], [], []])
+      []
+
+      iex> Benchee.Formatters.CSV.Util.zip_all([[], [], [4], []])
+      [[nil, nil, 4, nil]]
+  """
+  def zip_all(list_of_lists) do
+    do_zip(list_of_lists, [])
+  end
+
+  defp do_zip(list_of_lists, acc) do
+    converter = fn list, acc -> do_zip_each(list, acc) end
+    {remaining, heads} = :lists.mapfoldl(converter, [], list_of_lists)
+    do_zip_recur(remaining, heads, acc)
+  end
+
+  defp do_zip_each([], acc), do: {[], [nil | acc]}
+  defp do_zip_each([head | tail], acc), do: {tail, [head | acc]}
+
+  defp do_zip_recur(remaining, heads, acc) do
+    if Enum.all?(heads, &is_nil/1) do
+      Enum.reverse(acc)
+    else
+      do_zip(remaining, [Enum.reverse(heads) | acc])
+    end
+  end
+end
